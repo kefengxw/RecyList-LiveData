b3de755379736b3da6a474e40aeaad7121c494cb
kefengxw <kefengxw@gmail.com>
2018-12-04 04:36:40 +0100

    Initial commit

---
+++ app/src/main/java/com/rebtel/android/CountryBean.java
@@ -0,0 +1,16 @@
+package com.rebtel.android;
+
+import java.util.List;
+
+public class CountryBean {
+
+    /**
+     * name : Afghanistan
+     * alpha2Code : AF
+     * callingCodes : ["93"]
+     */
+
+    public String name;
+    public String alpha2Code;
+    public List<String> callingCodes;
+}
