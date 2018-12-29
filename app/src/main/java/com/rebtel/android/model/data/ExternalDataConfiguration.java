a25f558f8e0e89b9caef9b1c36a6a8fd8b1216c5
kefengxw <kefengxw@gmail.com>
2018-12-29 16:07:30 +0100

    add comments

--- app/src/main/java/com/rebtel/android/model/data/ExternalDataConfiguration.java
+++ app/src/main/java/com/rebtel/android/model/data/ExternalDataConfiguration.java
@@ -2,7 +2,7 @@ package com.rebtel.android.model.data;
 
 public class ExternalDataConfiguration {
 
-    public static final String BASE_URL = "https://restcountries.eu/rest/v2/";
+    public static final String BASE_URL = "https://restcountries.eu/rest/v2/";//the last one should be "/"
     public static final String DEFAULT_FLAG = "se";
     public static final String DEFAULT_CALL_CODE = "46";
 
